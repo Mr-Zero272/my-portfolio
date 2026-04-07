08203025011c4b7f84a03edd66f69a41afe2042c
Mr-Zero272 <pitithuong@gmail.com>
2026-04-07 09:51:05 +0700

    feat: temp add allow all CORS

--- app/api/[...slug]/route.ts
+++ app/api/[...slug]/route.ts
@@ -1,11 +1,31 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ message: 'API route not found' }, { status: 404 });
+  return NextResponse.json(
+    { message: 'API route not found' },
+    {
+      status: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    },
+  );
 }
 
 export async function POST() {
-  return NextResponse.json({ message: 'API route not found' }, { status: 404 });
+  return NextResponse.json(
+    { message: 'API route not found' },
+    {
+      status: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    },
+  );
 }
 
 // thêm PUT, DELETE nếu cần
