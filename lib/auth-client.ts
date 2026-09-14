0af6da56c3f7a7f49d421de7ced88ed7858771eb
Mr-Zero272 <pitithuong@gmail.com>
2026-09-14 10:39:49 +0700

    feat: update base URL in authClient to use env instead of hard code url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'https://pitithuong.dev.localhost',
+  baseURL: process.env.NEXT_PUBLIC_SITE_URL,
 });
 
 export const { signIn, signUp, useSession } = createAuthClient();
